228f114192f44964a228afbaea5b345e1deb98b7
dozyio <github@dozy.io>
2025-03-07 00:28:48 +0000

    chore: remove protocol-middleware package

--- packages/protocol-middleware/inspect-service.js
+++
@@ -1,3 +0,0 @@
-// This is a utility script for debugging purposes
-// eslint-disable-next-line no-console, no-undef
-console.log('Service structure:', JSON.stringify(Object.getOwnPropertyNames(components.identify), null, 2))
